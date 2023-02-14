c434f83283f5aeb0733a51d06ffa83cd32f10844
GonzaloEspinoza <gonis1990@gmail.com>
2023-02-14 00:27:59 -0400

    refactoring(cards):border and image

--- src/components/secctions/detaild_services/data.js
+++ src/components/secctions/detaild_services/data.js
@@ -1,9 +1,15 @@
+import card1 from '../../../../public/cards/card1.png';
+import card2 from '../../../../public/cards/card2.png'
+import card3 from '../../../../public/cards/card3.png'
+import card4 from '../../../../public/cards/card4.png'
+
 const dataDerechoF = [
     {
         title:'¿Buscas asistencia en temas de familia? ',
         icon:'',
         question:'¿Buscas asistencia en temas de familia?',
         description:"En momentos difíciles relacionados con el derecho de familia, es importante tener un abogado de confianza en tu lado. Nos especializamos en ofrecer asesoramiento legal personalizado y representación efectiva para ayudarte a navegar por cualquier desafío.",
+        image:card4,
         direccion:{
             icon:<i className="fa-sharp fa-solid fa-location-dot"></i>,
             location:'Potosí - Potosí, Bolivia',
@@ -19,6 +25,7 @@ const dataDerechoF = [
         icon:'',
         question:'¿Buscas asistencia en temas de familia?',
         description:"Nuestro equipo de abogados especializados te brinda una representación de alta calidad para ayudarte a proteger tus derechos y resolver cualquier problema de manera efectiva.",
+        image:card2,
         direccion:{
             icon:<i className="fa-sharp fa-solid fa-location-dot"></i>,
             location:'Potosí - Potosí, Bolivia',
@@ -34,6 +41,7 @@ const dataDerechoF = [
         icon:'',
         question:'¿Buscas asistencia en temas de familia?',
         description:"Nos especializamos en derecho de familia y ofrecemos asesoramiento y representación legal en temas como el divorcio, la custodia de los hijos, la distribución de bienes y mucho más. Contáctanos para obtener más información sobre cómo podemos ayudarte.",
+        image:card3,
         direccion:{
             icon:<i className="fa-sharp fa-solid fa-location-dot"></i>,
             location:'Potosí - Potosí, Bolivia',
@@ -49,6 +57,7 @@ const dataDerechoF = [
         icon:'',
         question:'¿Buscas asistencia en temas de familia?',
         description:"Si estás buscando un abogado que se especialice en derecho de familia en Bolivia, mira no más. Nuestro equipo está compuesto por abogados altamente capacitados y dedicados a ayudarte a proteger tus derechos y resolver cualquier problema de manera rápida y efectiva.",
+        image:card1,
         direccion:{
             icon:<i className="fa-sharp fa-solid fa-location-dot"></i>,
             location:'Potosí - Potosí, Bolivia',
--- src/components/secctions/detaild_services/derecho_familia.js
+++ src/components/secctions/detaild_services/derecho_familia.js
@@ -2,6 +2,8 @@ import derechoStyle from '../../../styles/derechoFalimia.module.css';
 import DATA from './data';
 import RAZONRES from './data_rasones';
 
+import Image from 'next/image';
+
 export default function DerechoFamilia(){
 
     return(
@@ -13,7 +15,9 @@ export default function DerechoFamilia(){
                     Nuestro equipo de expertos te brinda un apoyo integral y personalizado para ayudarte a resolver cualquier problema y proteger tus derechos. Ya sea que necesites asesoramiento en temas de custodia, divorcio, distribución de bienes o cualquier otra cuestión relacionada con el derecho de familia, estamos aquí para ayudarte. Contáctanos hoy para obtener más información sobre cómo podemos ayudarte a navegar por cualquier desafío de manera efectiva
                 </p>
             </div>
+            <div className={derechoStyle.title_buscas_legal}><h2>BUSCAS ASISTENCIA LEGAL</h2></div>
             <div className={derechoStyle.listCards}>
+            
                 {
                     DATA.map((d,i)=>{
                         return(
@@ -24,7 +28,7 @@ export default function DerechoFamilia(){
                                     ?''
                                     :
                                     <div  className={derechoStyle.title}>
-                                    <h3>¿Buscas asistencia en temas de familia?</h3>
+                                    {/* <h3>¿Buscas asistencia en temas de familia?</h3> */}
                                 </div>
                                 }
                                 <div className={derechoStyle.description}>
@@ -39,13 +43,18 @@ export default function DerechoFamilia(){
                                     <div>Potosí - Potosí, Bolivia</div>
                                 </div>
                             </div>
-                            <div className={derechoStyle.contactContent}>
+                            {/* <div className={derechoStyle.contactContent}>
                                 <div className={derechoStyle.icon}>
                                     <i className="fa-brands fa-whatsapp"></i>
                                 </div>
                                 <div className={derechoStyle.contact}>
                                     Contactar
                                 </div>
+                            </div> */}
+                            <div className={derechoStyle.conten_imga_card}>
+                                <Image
+                                    src={d.image}
+                                ></Image>
                             </div>
                         </div>);
                     })
